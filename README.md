64cf9883ffa6b0a99a2c60195a403b6eb87177fc
ViniciusResendeDaCruz <vinicius.resende.cruz@gmail.com>
2022-11-25 17:23:30 -0300

    Criação do projeto e inclusão de plugins

---
+++ README.md
@@ -0,0 +1,16 @@
+# Alunos App
+
+## O sistema
+
+Sistema feito em PHP com CodeIgniter, com uma tela inicial de menu que leva para um crud de aluno com os campos : nome, endereço e foto. O crud deve permitir o upload da imagem em JPG. 
+Para a entrega, você deve enviar o código fonte completo da sua solução e instruções de instalação. O local de armazenamento das informações deve ser escolhido por você. 
+Extras não obrigatórios : disponibilizar o código na web, permitindo o teste completo da aplicação desenvolvida.
+
+## Instalação
+
+ • O sistema necessita de um servidor local funcionando(**WAMP** recomendado)
+
+ • Um arquivo de banco de dados **MySQL** está disponível dentro da pasta public/database, importe-o no seu servidor MySQL
+
+ • Use o comando **`composer update`**
+
---
+++ alunosapp.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Nov 25, 2022 at 07:58 PM
+-- Server version: 5.7.36
+-- PHP Version: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `alunosapp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `alunos`
+--
+
+DROP TABLE IF EXISTS `alunos`;
+CREATE TABLE IF NOT EXISTS `alunos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `endereco` varchar(100) NOT NULL,
+  `fotoPerfil` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
